4de8cdb7b826d99565fa7b92580b3b716a262544
itseuclid <103278154+itseuclid@users.noreply.github.com>
2022-09-21 06:35:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Proctoglyvenol's</title>
+  <meta content="" name="Proctoglyvenol's workshop & portfolio web site.">
+  <meta content="" name="minecraft, itemsadder, fontimages, models, blockbench, portfolio, workshop">
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= ANASAYFA/BASLIK ======= -->
+  <header id="header">
+    <div class="container">
+      <h1 style="margin-top: 20px;">Proctoglyvenol's</h1>
+      <nav style="margin-top: 10px; margin-left: 5px;" id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="index.php">Homepage</a></li>
+          <li><a class="nav-link" href="workshop.php">Workshop</a></li>
+          <li><a class="nav-link" href="about.php">About Me</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+      <div style="margin-top: 30px; margin-left: 5px;" class="social-links">
+        <a href="https://discord.gg/E3Gytk3AbY" class="discord"><i class="bi bi-discord"></i></a>
+      </div>
+    </div>
+  </header>
+  <div class="credits">
+    by <a href="https://www.instagram.com/itseuclid/">euclidbeen</a>
+  </div>
+
+  <!-- SCRIPTLER -->
+  
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/main.js" type="text/javascript"></script>
+  <script src="assets/js/ip.js" type="text/javascript"></script>
+
+</body>
+
+</html>
